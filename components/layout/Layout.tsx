10e59f0acc30e18928082ad84d5fe2fa872df1b9
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-06-01 15:09:54 -0700

    .

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => (
+  <>
+    <Header />
+    <main className="min-h-screen">{children}</main>
+    <Footer />
+  </>
+);
+
+export default Layout;
