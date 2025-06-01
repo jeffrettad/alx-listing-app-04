8e862723db2b149e3514ce232219b2f7cde10d2b
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-06-01 15:15:11 -0700

    .

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      {/* Hero Section */}
+      <div className="bg-[url('/path/to/bg.jpg')] bg-cover bg-center h-[300px] flex flex-col justify-center items-center text-white text-center">
+        <h1 className="text-4xl font-bold">Find your favorite place here!</h1>
+        <p className="text-lg mt-2">The best prices for over 2 million properties worldwide.</p>
+      </div>
+    </div>
+  );
+}
