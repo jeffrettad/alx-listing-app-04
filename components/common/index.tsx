4fca73b2631f5214070cbdbac63948bb7d8f4331
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-06-29 05:38:59 -0700

    .

---
+++ components/common/index.tsx
@@ -0,0 +1,17 @@
+import Pill from "@/components/common/pill";
+
+const filters = ["Top Villa", "Self Checkin", "Free Parking", "Pet Friendly"];
+
+<div className="p-4 flex flex-wrap">
+  {filters.map((filter) => (
+    <Pill key={filter} label={filter} />
+  ))}
+</div>
+import { PROPERTYLISTINGSAMPLE } from "@/constants";
+import PropertyCard from "@/components/common/PropertyCard";
+
+<div className="flex flex-wrap justify-center p-4">
+  {PROPERTYLISTINGSAMPLE.map((property) => (
+    <PropertyCard key={property.name} property={property} />
+  ))}
+</div>
