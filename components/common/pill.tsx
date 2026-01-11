92258b4b3f383ed69c38d7b14d16cfd1f0d50a4b
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2026-01-11 11:02:58 -0800

    Update pill.tsx

--- components/common/pill.tsx
+++ components/common/pill.tsx
@@ -1,4 +1,4 @@
-const Pill = ({ label }: { label: string }) => (
+gconst Pill = ({ label }: { label: string }) => (
   <span className="px-4 py-1 bg-blue-100 text-blue-800 rounded-full text-sm mr-2 mb-2 inline-block cursor-pointer hover:bg-blue-200">
     {label}
   </span>
